b87e45a65d7219e83234b33825fed485fbb7f71d
Jam Sávio <jamsaavio@gmail.com>
2025-07-09 14:12:47 -0300

    Update language_feedbacks.js

--- assets/js/language_feedbacks.js
+++ assets/js/language_feedbacks.js
@@ -1,8 +1,8 @@
 var userLang = navigator.language || navigator.userLanguage;
 
 if(userLang.indexOf("pt-")==-1 && userLang.indexOf("es-")==-1){
-	document.getElementById("feedback-01").textContent="Savio is one of the most talented programmers I've ever had the pleasure of meeting, extremely punctual and very ethical. The guy is outstanding, very creative and revolutionary in his developments.";
-	document.getElementById("feedback-02").textContent="Jam Savio is a great professional as a programmer and as a human. Owner of a trustworthy and honest ethical posture. I've been working with him for some time now and I have no complaints. Programmer of those that, if it doesn't exist, he goes there and creates.";
-	document.getElementById("feedback-03").textContent="Savio is very innovative, everything he does is very exclusive and unique. Most projects are out of the ordinary and well organized.";
-	document.getElementById("feedback-04").textContent="Well, I started a project with Savio and he was very attentive and quickly understood my ideas. Any error that happened with the bot, any bug, it was resolved on the spot. Really fast. Savio is an excellent professional. I am satisfied and ready for the next projects. I recommend, you won't regret it!";
+	document.getElementById("feedback-01").textContent="Savio is one of the most talented programmers I have ever had the pleasure of working with. He is extremely punctual and highly ethical. This guy is outstanding—very creative and revolutionary in his developments.";
+	document.getElementById("feedback-02").textContent="Savio is a great professional both as a programmer and as a person. He has a trustworthy and honest ethical stance. I’ve been working with him for some time now, and I have no complaints. He’s the kind of programmer who, if something doesn’t exist, goes ahead and creates it.";
+	document.getElementById("feedback-03").textContent="Savio is very innovative; everything he does is exclusive and unique. Most of his projects are extraordinary and well-organized.";
+	document.getElementById("feedback-04").textContent="I started a project with Savio, and he was very attentive and quickly understood my ideas. Any errors with the bot, any bugs, were resolved on the spot—really fast. Savio is an excellent professional. I am satisfied and looking forward to the next projects. I highly recommend him—you won’t regret it!";
 }
